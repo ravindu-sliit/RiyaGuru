20a6dd64844ac345984e63fdf059d9c9af917caf
ravindu-sliit <it23829510@my.sliit.lk>
2025-09-18 00:51:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Server running 🚀 on port ${PORT} and connected to MongoDB`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
+  //
+  //
+  //
+  //
