dcd21247f26b883f8ece7a513d7296ddf5a16348
SandaniChamoda <sandanichamoda450@gmail.com>
2025-09-18 00:21:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     app.listen(process.env.PORT || 5000, () =>
-//       console.log("Server running")
-//     );
-//   })
-//   .catch((err) => console.error(err));
+mongoose.connect("mongodb+srv://RiyaGuru_lk:riyaguru_123@cluster0.nozx2ta.mongodb.net/RiyaGuruDB")
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () =>
+      console.log("Server running 🚀 connected to MongoDB")
+    );
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 app.listen(process.env.PORT || 5000, () => 
   console.log(`Server running on port ${process.env.PORT || 5000}`)
