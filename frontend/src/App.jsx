2d451b88deedc4cf929abe64304e11ad689d4e41
ravindu-sliit <it23829510@my.sliit.lk>
2025-09-21 22:32:27 +0530

    frontendstruct

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Routes } from "react-router";
 import "./App.css";
-import Payment from "./Components/Payment/Payment"; 
 
 function App() {
   return ( 
     <div>
       <React.Fragment>
         <Routes>
-              <Route path="/Payment" element={<Payment />} /> 
         </Routes>
       </React.Fragment>
     </div>
