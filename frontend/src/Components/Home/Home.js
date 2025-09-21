cd2ee068a6a6a4fa3641ee04e0235fe5777048af
SandaniChamoda <sandanichamoda450@gmail.com>
2025-09-21 12:33:18 +0530

    FrontEnd Structure
    this is floder structure for frontend

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../Nav/Nav"
+
+function Home() {
+  console.log("✅ Home component rendered");
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+        <Nav/>
+      <h1 style={{ color: "red", fontSize: "48px" }}>HELLO FROM HOME
+        I am Sanda
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/Components/UserDetails/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+function Users() {
+  return (
+    <div>
+     
+      <h1>Users Details</h1>
+    </div>
+  )
+}
+
+export default Users
