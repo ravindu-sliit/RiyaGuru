2ecdd2052b39b5ed60fb564bf5348034b5594b68
ravindu-sliit <it23829510@my.sliit.lk>
2025-09-23 18:01:35 +0530

    Update AppRoutes.js

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,6 +1,5 @@
 import {  Routes, Route, Navigate } from "react-router-dom";
 import DriveManagerLanding from "../pages/DriveManagerLanding";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/Auth/LoginPage";
 import StudentDashboard from "../pages/Student/StudentDashboard";
 import InstructorDashboard from "../pages/Instructor/InstructorDashboard";
