8cbcc937867a9a14a6f096a17a28793734f37cd1
NSadeesha <nethmisadeesha2002@gmail.com>
2025-09-24 01:30:53 +0530

    Create validateRequest.js

---
+++ backend/middleware/validateRequest.js
@@ -0,0 +1,18 @@
+// backend/middleware/validateRequest.js
+import { validationResult } from "express-validator";
+
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array().map(err => ({
+        field: err.param,
+        message: err.msg,
+      })),
+    });
+  }
+  next();
+};
+
+export default validateRequest;
